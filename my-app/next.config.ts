4df5b4ca4ee83177a215ab4df8c86b1a32c9f1ed
Robert <evise.apply@gmail.com>
2025-12-05 23:43:32 +0330

    fix webpack config to make sure the build runs smoothly

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,37 +1,14 @@
 import type { NextConfig } from "next";
-import type { RuleSetRule } from "webpack";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    rules: {
-      "*.svg": {
-        loaders: ["@svgr/webpack"],
-        as: "*.js",
-      },
+    turbopack: {
+        rules: {
+            // Turbopack-native SVG -> React component loader
+            "*.svg": {
+                loaders: ["@svgr/webpack"],
+            },
+        },
     },
-  },
-  webpack: (config) => {
-    const rules = config.module.rules as RuleSetRule[];
-    const svgRule = rules.find(
-      (rule): rule is RuleSetRule & { test: RegExp } =>
-        typeof rule === "object" &&
-        rule !== null &&
-        rule.test instanceof RegExp &&
-        rule.test.test(".svg"),
-    );
-
-    if (svgRule) {
-      svgRule.exclude = /\.svg$/i;
-    }
-
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: [{ loader: "@svgr/webpack", options: { titleProp: true } }],
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
